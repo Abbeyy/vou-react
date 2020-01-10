5d8f2c42a73a6376103bec828ebc78d2f0851dfb
Abigail Ross <abbey@pc-74-224.customer.ask4.lan>
2020-01-10 15:00:57 +0000

    Gathering login fields into state

--- vou/src/components/Login.js
+++ vou/src/components/Login.js
@@ -8,22 +8,30 @@ class Login extends React.Component {
         this.props = props;
         this.state = {username: '', password: ''};
     }
+
+    onChangeText = (e) => {
+        if (e.name === 'username') {
+            this.setState({username: e.target.value});
+        } else {
+            this.setState({password: e.target.value}); //encrypt within app?
+        }
+    }
+
     render() {
         return (
             <div className="Login">
                 <Form name="loginForm">
                     <Form.Group name="usernameGroup" controlId="formBasicEmail">
                         <Form.Label>Username</Form.Label>
-                        <Form.Control type="text" placeholder="Enter Username" />
+                        <Form.Control name="username" type="text" placeholder="Enter Username" onChange={this.onChangeText}/>
                     </Form.Group>
                     <Form.Group name="passwordGroup" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-
+                        <Form.Control name="password" type="password" placeholder="Password" onChange={this.onChangeText}/>
                     </Form.Group>
-                    <Form.Group name="keepLoggedInGroup" controlId="formBasicCheckbox">
+                    {/* <Form.Group name="keepLoggedInGroup" controlId="formBasicCheckbox">
                         <Form.Check type="checkbox" label="Keep me logged in?" />
-                    </Form.Group>
+                    </Form.Group> */}
                     <Button variant="primary" type="submit">
                         Log In
                     </Button>
